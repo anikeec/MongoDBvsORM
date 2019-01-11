abd88f8adfcb9b4614051cb82e4f38aa7179a6d5
apu <pasha_anik@ukr.net>
2019-01-11 21:20:04 +0200

    Fix bug - bd name

---
+++ src/main/java/com/apu/mongodbvsorm/menu/delete/DeleteByParameterMenuState.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.apu.mongodbvsorm.menu.find;
+
+import com.apu.mongodbvsorm.TempRepository;
+import com.apu.mongodbvsorm.entities.NotebookEntity;
+import com.apu.mongodbvsorm.menu.MenuState;
+
+/**
+ *
+ * @author apu
+ */
+public class FindByParameterMenuState extends MenuState {
+    
+    private static MenuState instance;
+
+    private FindByParameterMenuState() {
+    }
+    
+    @Override
+    public String handle() {
+        System.out.println(getMenuHeader());    //print menu text 
+        String inputText;
+        while(true) {                           //get input data
+            inputText = this.getInputText();
+            if(inputText.equals("")) {
+                System.out.println("Please enter parameter name:");
+                continue;
+            } else {
+                break;
+            }
+        }        
+        NotebookEntity tempEntity = new NotebookEntity();
+        tempEntity.getParameters().put(inputText, null);
+        TempRepository.setEntityToSave(tempEntity);
+//        MenuState.setPreviousState(this);
+        MenuState.setCurrentState(FindProcessMenuState.getInstance());
+        return MenuState.RET_OK;
+    }
+
+    @Override
+    public String getMenuHeader() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Enter parameter name:");
+        return sb.toString();
+    }
+    
+    public static MenuState getInstance() {
+        if(instance == null)
+            instance = new FindByParameterMenuState();
+        return instance;
+    }
+    
+}
---
+++ src/main/java/com/apu/mongodbvsorm/menu/delete/DeleteProcessMenuState.java
@@ -0,0 +1,86 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.apu.mongodbvsorm.menu.find;
+
+import com.apu.mongodbvsorm.TempRepository;
+import com.apu.mongodbvsorm.dao.NotebookEntityDAO;
+import com.apu.mongodbvsorm.entities.NotebookEntity;
+import com.apu.mongodbvsorm.menu.MainMenuState;
+import com.apu.mongodbvsorm.menu.MenuState;
+import com.apu.mongodbvsorm.menu.add.AddSavingMenuState;
+import com.apu.mongodbvsorm.utils.Logger;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import org.apache.commons.lang.exception.ExceptionUtils;
+import org.mongodb.morphia.dao.BasicDAO;
+import org.mongodb.morphia.query.Query;
+
+/**
+ *
+ * @author apu
+ */
+public class FindProcessMenuState  extends MenuState {
+
+    private static Logger LOGGER = Logger.getInstance();
+    private static MenuState instance;
+
+    private FindProcessMenuState() {
+    }
+    
+    @Override
+    public String handle() {
+        NotebookEntity tempEntity = TempRepository.getEntityToSave();
+        if(tempEntity == null)
+            throw new NullPointerException("tempEntity hasn't initialized yet.");
+        
+        BasicDAO dao = NotebookEntityDAO.getInstance();
+        
+        Query<NotebookEntity> query = dao.createQuery();
+        Map<String, String> parameters = tempEntity.getParameters();
+        if(!parameters.isEmpty()) {
+            Iterator<Map.Entry<String, String>> iterator = 
+                            parameters.entrySet().iterator();
+            while(iterator.hasNext()) {
+                Map.Entry<String, String> entry = iterator.next();
+                String key = entry.getKey();
+                String value = entry.getValue();
+                if((key != null)&&(value != null))
+                    query = query.field(key).equal(value);
+                else if((key != null)&&(value == null))          
+                    query = query.field("parameters." + key).exists();
+            }
+        }
+        List<NotebookEntity> results;
+        try {
+            results = dao.find(query).asList();
+            System.out.println("Search results:");
+            for(NotebookEntity entity:results) {
+                System.out.println(entity.toString());
+            }
+        } catch (Exception ex) {
+            LOGGER.error(AddSavingMenuState.class, ExceptionUtils.getStackTrace(ex));
+        }
+        TempRepository.setTempParameter(null);
+        TempRepository.setEntityToSave(null);
+        
+        System.out.println();
+        MenuState.setCurrentState(MainMenuState.getInstance());
+        return MenuState.RET_OK;
+    }
+
+    @Override
+    public String getMenuHeader() {
+        return "";
+    }
+    
+    public static MenuState getInstance() {
+        if(instance == null)
+            instance = new FindProcessMenuState();
+        return instance;
+    }
+    
+}
