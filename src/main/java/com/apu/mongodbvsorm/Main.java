31190ebd6cb61622692a4070a0142d0655bbad73
apu <pasha_anik@ukr.net>
2019-01-11 19:51:08 +0200

    Refactoring - change connection settings

--- src/main/java/com/apu/mongodbvsorm/Main.java
+++ src/main/java/com/apu/mongodbvsorm/Main.java
@@ -6,14 +6,12 @@
 package com.apu.mongodbvsorm;
 
 import com.apu.mongodbvsorm.dao.NotebookEntityDAO;
-import com.apu.mongodbvsorm.entities.Address;
-import com.apu.mongodbvsorm.entities.Person;
 import com.apu.mongodbvsorm.menu.MainMenuState;
 import com.apu.mongodbvsorm.menu.MenuState;
-import com.apu.mongodbvsorm.utils.Logger;
 import com.mongodb.MongoClient;
-import java.util.List;
-import javax.management.Query;
+import com.mongodb.MongoClientOptions;
+import com.mongodb.ReadPreference;
+import com.mongodb.ServerAddress;
 import org.mongodb.morphia.Datastore;
 import org.mongodb.morphia.Morphia;
 
@@ -23,19 +21,12 @@ import org.mongodb.morphia.Morphia;
  */
 public class Main {
     
+    public static final String DB_HOST = "127.0.0.1";
+    public static final int DB_PORT = 27017;
+    public static final String DB_NAME = "NotebookDB";
+    
     public static void main(String[] args) {
-        
-//        final Morphia morphia = new Morphia();
-//        final Datastore datastore = morphia.createDatastore(new MongoClient(), "morphia");
-//        morphia.mapPackage("com.apu.mongodbvsorm.entities");
-////        morphia.map(Address.class);
-//        morphia.mapPackageFromClass(Address.class);
-//        Address addresss1 = new Address(1, "Popudrenko", "Chernigiv", "Ukraine");
-//        datastore.save(addresss1);
-//        
-//        List<Address> addresses = datastore.find(Address.class).asList();
-//        System.out.println("");
-        
+       
         Main.init();
 
         String ret = null;
@@ -43,7 +34,7 @@ public class Main {
         MenuState.setCurrentState(state);
         MenuState.setPreviousState(state);
         while(true) {
-            ret = state.getCurrentState().handle();
+            ret = MenuState.getCurrentState().handle();
             if(ret != null) {
                 if(ret.equals(MenuState.RET_EXIT))
                     return;
@@ -53,6 +44,19 @@ public class Main {
     }
     
     private static void init() {
+        MongoClientOptions mongoOptions = MongoClientOptions.builder()
+                // Wait 1m for a query to finish, https://jira.mongodb.org/browse/JAVA-1076
+            .socketTimeout(60000) 
+                // Try the initial connection for 15s, http://blog.mongolab.com/2013/10/do-you-want-a-timeout/
+            .connectTimeout(15000) 
+                // Keep idle connections for 10m, so we discard failed connections quickly
+            .maxConnectionIdleTime(600000) 
+                // Read from the primary, if not available use a secondary
+            .readPreference(ReadPreference.primaryPreferred()) 
+            .build();
+        MongoClient mongoClient;
+        mongoClient = new MongoClient(new ServerAddress(DB_HOST, DB_PORT), mongoOptions);
+        
         final Morphia morphia = new Morphia();
         morphia.mapPackage("com.apu.mongodbvsorm.entities");
         final Datastore datastore = morphia.createDatastore(new MongoClient(), "notebookDB");        
---
+++ src/main/java/com/apu/mongodbvsorm/menu/find/FindByParameterMenuState.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.apu.mongodbvsorm.menu.add;
+
+import com.apu.mongodbvsorm.TempRepository;
+import com.apu.mongodbvsorm.entities.Parameter;
+import com.apu.mongodbvsorm.menu.MenuState;
+
+/**
+ *
+ * @author apu
+ */
+public class AddParameterMenuState extends MenuState {
+    
+    private static MenuState instance;
+
+    private AddParameterMenuState() {
+    }
+    
+    @Override
+    public String handle() {
+        System.out.println(getMenuHeader());    //print menu text 
+        String inputText;
+        while(true) {                           //get input data
+            inputText = this.getInputText();
+            if(inputText.equals("")) {
+                System.out.println("Please enter parameter name:");
+                continue;
+            } else {
+                break;
+            }
+        }
+        Parameter tempParameter = new Parameter();
+        tempParameter.setName(inputText);
+        TempRepository.setTempParameter(tempParameter);
+//        MenuState.setPreviousState(this);
+        MenuState.setCurrentState(AddParameterValueMenuState.getInstance());
+        return MenuState.RET_OK;
+    }
+
+    @Override
+    public String getMenuHeader() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Enter parameter name:");
+        return sb.toString();
+    }
+    
+    public static MenuState getInstance() {
+        if(instance == null)
+            instance = new AddParameterMenuState();
+        return instance;
+    }
+    
+}
